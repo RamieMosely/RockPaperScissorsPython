9ca0e6a2083048adabd5ee289af96211cc267866
Ramie Mosely <52608883+RamieMosely@users.noreply.github.com>
2024-10-03 12:33:36 -0400

    added user choice computer choice

--- rpsGame.py
+++ rpsGame.py
@@ -1,10 +1,33 @@
 #Ramie Mosely Rock Papers Scissors In Python
-
+#Importing Modules
 import random
+import math
 
-def play():
+#Getting user input and computer choice
+def playGame():
     user = input("Choose Your Weapon: 'r' for ROCKZ! - 'p' for PAPERZ! - 's' for SCISSORZ!")
     user = user.lowerr()
 
-    computer = random.choice(['r', 'p', 's'])
+    computerChoice = range(1,4)
+
+    computerRange = random.choice(computerChoice)
+
+    if computerRange == 1:
+        compFinal = 'r'
+    elif computerRange == 2:
+        compFinal = 'p'
+    else:
+        compFinal = 's'
+
+    if user == compFinal:
+        return "You and the computer have both chosen {}. It is a tie".format(compFinal) 
+    
+    if win(user,compFinal):
+        return "You have chosen {} and the computer has chosen {}. You won!".format(user, compFinal)
+
+
+
+#Check if the player won
+def win(player, enemy):
+
 
